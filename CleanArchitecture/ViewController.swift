a54a24b7109505b33053683c2347764496d86852
Myung Hoon So <joa214@hotmail.com>
2024-04-21 12:58:16 +0100

    first commit

---
+++ CleanArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanArchitecture
+//
+//  Created by myung hoon on 21/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
